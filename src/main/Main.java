a3ba2426990be60e65b5d879829c05cf48421686
LiveQuince <111811025+LiveQuince@users.noreply.github.com>
2024-05-23 11:50:22 +0700

    hehehe

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,6 @@ package main;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Pantheekkkk");
+        System.out.println("Hello, World!");      
     }
 }
