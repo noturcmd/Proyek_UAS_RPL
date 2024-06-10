432810040bce00613849c659049c18f929b322e3
AlonePainPeanut <139093156+noturcmd@users.noreply.github.com>
2024-06-10 15:06:19 +0700

    yeh

--- src/GUI/MenuAdmin.java
+++ src/GUI/MenuAdmin.java
@@ -32,11 +32,11 @@ public class MenuAdmin extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1401, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 1117, Short.MAX_VALUE)
         );
 
         pack();
